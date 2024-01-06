3220658e6656f0945fdcb18242f13af05874ef5b
Soon Jae <ryupark1321@gmail.com>
2024-01-06 15:58:57 -0500

    Creating gitignore

--- dist/App.js
+++ dist/App.js
@@ -5,11 +5,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
-const port = 30000;
+const port = 3000;
 app.get("/", function (req, res) {
     res.send("Hello");
 });
 app.listen(port, () => {
-    console.log("Server is running at ${port}");
+    console.log('Server is running at ${port}');
 });
 //# sourceMappingURL=App.js.map
--- src/App.ts
+++ src/App.ts
@@ -1,12 +1,12 @@
 import express, { Express, Request, Response } from "express";
 
 const app: Express = express();
-const port: number = 30000;
+const port: number = 3000;
 
 app.get("/", function (req: Request, res: Response) {
   res.send("Hello");
 });
 
 app.listen(port, () => {
-	console.log("Server is running at ${port}");
+	console.log(`Server is running at ${port}`);
 });
